284eeb5bdd5d8a27e9dda5fd4ff73362f2c2b073
Ekaterina <ketee.android@yahoo.com>
2021-01-14 22:45:54 +0300

    Homework 4 is done

--- app/src/main/java/com/ketee_jishs/weather_application/AboutActivity.java
+++ app/src/main/java/com/ketee_jishs/weather_application/AboutActivity.java
@@ -1,20 +1,13 @@
 package com.ketee_jishs.weather_application;
 
-import androidx.annotation.RequiresApi;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity{
 
     private Button okButtonAbout;
 
@@ -23,51 +16,6 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        Toast.makeText(this, "onCreateAbout", Toast.LENGTH_SHORT).show();
-        Log.d("onCreate", "onCreateAbout");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "onStartAbout", Toast.LENGTH_SHORT).show();
-        Log.d("onStart", "onStartAbout");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "onResumeAbout", Toast.LENGTH_SHORT).show();
-        Log.d("onResume", "onResumeAbout");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "onPauseAbout", Toast.LENGTH_SHORT).show();
-        Log.d("onPause", "onPauseAbout");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "onStopAbout", Toast.LENGTH_SHORT).show();
-        Log.d("onStop", "onStopAbout");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(this, "onRestartAbout", Toast.LENGTH_SHORT).show();
-        Log.d("onRestart", "onRestartAbout");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "onDestroyAbout", Toast.LENGTH_SHORT).show();
-        Log.d("onDestroy", "onDestroyAbout");
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
--- app/src/main/java/com/ketee_jishs/weather_application/MainActivity.java
+++ app/src/main/java/com/ketee_jishs/weather_application/MainActivity.java
@@ -1,56 +1,44 @@
 package com.ketee_jishs.weather_application;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
+import android.net.Uri;
 import android.os.Bundle;
-import android.text.Html;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity{
     private final String dayTempDataKey = "dayTempDataKey";
     private final String nightTempDataKey = "nightTempDataKey";
-//    private final String atmospherePressureDataKey = "atmospherePressureDataKey";
-//    private final String windSpeedDataKey = "windSpeedDataKey";
-//    private final String humidityDataKey = "humidityDataKey";
+    private final String atmospherePressureDataKey = "atmospherePressureDataKey";
+    private final String windSpeedDataKey = "windSpeedDataKey";
+    private final String humidityDataKey = "humidityDataKey";
+    private final String chosenCityDataKey = "chosenCityDataKey";
+    private final String weekWeatherDataKey = "weekWeatherDataKey";
+
+    private final int choseCityActivityRequestCode = 15;
 
     private TextView dayDegreesView;
     private TextView nightDegreesView;
     private TextView atmospherePressureInfoView;
     private TextView windSpeedInfoView;
     private TextView humidityInfoView;
+    private TextView chosenCityView;
+    private TextView weekWeatherUrlView;
 
     private Button cityButton;
     private Button settingsButton;
-    private Button windSpeedCounterButton;
-    private Button humidityCounterButton;
-    private Button pressureCounterButton;
-    private Button nightTempCounterButton;
-    private Button dayTempCounterButton;
-
-    private AtmospherePressureDataContainer atmospherePressureDataContainer = AtmospherePressureDataContainer.getInstance();
-    private WindSpeedDataContainer windSpeedDataContainer = WindSpeedDataContainer.getInstance();
-    private HumidityDataContainer humidityDataContainer = HumidityDataContainer.getInstance();
+    private Button weekWeatherButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
-        ActionBar actionBar = getSupportActionBar();
-
-        Toast.makeText(this, "onCreateMain", Toast.LENGTH_SHORT).show();
-        Log.d("onCreate", "onCreateMain");
 
         cityButton.setOnClickListener(v -> {
             onClickSetCity(cityButton);
@@ -60,107 +48,49 @@ public class MainActivity extends AppCompatActivity {
             onClickSettings(settingsButton);
         });
 
-        windSpeedCounterButton.setOnClickListener(v -> {
-            onClickWind(windSpeedCounterButton);
-        });
-
-        humidityCounterButton.setOnClickListener(v -> {
-            onClickHumidity(humidityCounterButton);
-        });
-
-        pressureCounterButton.setOnClickListener(v -> {
-            onClickPressure(pressureCounterButton);
-        });
-
-        nightTempCounterButton.setOnClickListener(v -> {
-            onClickNightTemp(nightTempCounterButton);
-        });
-
-        dayTempCounterButton.setOnClickListener(v -> {
-            onClickDayTemp(dayTempCounterButton);
+        weekWeatherButton.setOnClickListener(v -> {
+            onClickWeekWeather(weekWeatherButton);
         });
     }
 
     private void findViews() {
 //        TextViews
         dayDegreesView = (TextView) findViewById(R.id.dayDegreesView);
-        cityButton = (Button)findViewById(R.id.cityButton);
         nightDegreesView = (TextView) findViewById(R.id.nightDegreesView);
         atmospherePressureInfoView = (TextView) findViewById(R.id.atmospherePressureInfoView);
         windSpeedInfoView = (TextView) findViewById(R.id.windSpeedInfoView);
         humidityInfoView = (TextView) findViewById(R.id.humidityInfoView);
+        chosenCityView = (TextView) findViewById(R.id.chosenCityView);
+        weekWeatherUrlView = (TextView) findViewById(R.id.weekWeatherUrlView);
 
 //        Buttons
         settingsButton = (Button)findViewById(R.id.settingsButton);
-        windSpeedCounterButton = (Button)findViewById(R.id.windSpeedCounterButton);
-        humidityCounterButton = (Button)findViewById(R.id.humidityCounterButton);
-        pressureCounterButton = (Button)findViewById(R.id.pressureCounterButton);
-        nightTempCounterButton = (Button)findViewById(R.id.nightTempCounterButton);
-        dayTempCounterButton = (Button)findViewById(R.id.dayTempCounterButton);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "onStartMain", Toast.LENGTH_SHORT).show();
-        Log.d("onStart", "onStartMain");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "onResumeMain", Toast.LENGTH_SHORT).show();
-        Log.d("onResume", "onResumeMain");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "onPauseMain", Toast.LENGTH_SHORT).show();
-        Log.d("onPause", "onPauseMain");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "onStopMain", Toast.LENGTH_SHORT).show();
-        Log.d("onStop", "onStopMain");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(this, "onRestartMain", Toast.LENGTH_SHORT).show();
-        Log.d("onRestart", "onRestartMain");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "onDestroyMain", Toast.LENGTH_SHORT).show();
-        Log.d("onDestroy", "onDestroyMain");
+        cityButton = (Button)findViewById(R.id.cityButton);
+        weekWeatherButton =(Button)findViewById(R.id.weekWeatherButton);
     }
 
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
-        Toast.makeText(this, "onSaveInstanceStateMain", Toast.LENGTH_SHORT).show();
-        Log.d("onSaveInstanceState", "onSaveInstanceStateMain");
-
         String dayTemp = dayDegreesView.getText().toString();
         outState.putString(dayTempDataKey, dayTemp);
 
         String nightTemp = nightDegreesView.getText().toString();
         outState.putString(nightTempDataKey, nightTemp);
 
-        //Using Singleton
         String atmospherePressure = atmospherePressureInfoView.getText().toString();
-        outState.putString(String.valueOf(atmospherePressureDataContainer), atmospherePressure);
+        outState.putString(atmospherePressureDataKey, atmospherePressure);
 
         String windSpeed = windSpeedInfoView.getText().toString();
-        outState.putString(String.valueOf(windSpeedDataContainer), windSpeed);
+        outState.putString(windSpeedDataKey, windSpeed);
 
         String humidity = humidityInfoView.getText().toString();
-        outState.putString(String.valueOf(humidityDataContainer), humidity);
+        outState.putString(humidityDataKey, humidity);
+
+        String city = chosenCityView.getText().toString();
+        outState.putString(chosenCityDataKey, city);
+
+        String weekWeatherUrl = weekWeatherUrlView.getText().toString();
+        outState.putString(weekWeatherDataKey, weekWeatherUrl);
 
         super.onSaveInstanceState(outState);
     }
@@ -169,61 +99,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
 
-        Toast.makeText(this, "onRestoreInstanceStateMain", Toast.LENGTH_SHORT).show();
-        Log.d("onSaveInstanceState", "onRestoreInstanceStateMain");
-
         String dayTemp = savedInstanceState.getString(dayTempDataKey);
         dayDegreesView.setText(dayTemp);
 
         String nightTemp = savedInstanceState.getString(nightTempDataKey);
         nightDegreesView.setText(nightTemp);
 
-        //Using Singleton
-        String atmospherePressure = savedInstanceState.getString(String.valueOf(atmospherePressureDataContainer));
+        String atmospherePressure = savedInstanceState.getString(atmospherePressureDataKey);
         atmospherePressureInfoView.setText(atmospherePressure);
 
-        String windSpeed = savedInstanceState.getString(String.valueOf(windSpeedDataContainer));
+        String windSpeed = savedInstanceState.getString(windSpeedDataKey);
         windSpeedInfoView.setText(windSpeed);
 
-        String humidity = savedInstanceState.getString(String.valueOf(humidityDataContainer));
+        String humidity = savedInstanceState.getString(humidityDataKey);
         humidityInfoView.setText(humidity);
+
+        String city = savedInstanceState.getString(chosenCityDataKey);
+        chosenCityView.setText(city);
+
+        String weekWeatherUrl = savedInstanceState.getString(weekWeatherDataKey);
+        weekWeatherUrlView.setText(weekWeatherUrl);
     }
 
     public void onClickSetCity(View view) {
-        startActivity(new Intent(MainActivity.this, ChoseCityActivity.class));
+        Intent chosenCity = new Intent(MainActivity.this, ChoseCityActivity.class);
+        startActivityForResult(chosenCity, choseCityActivityRequestCode);
     }
 
     public void onClickSettings(View view) {
         startActivity(new Intent(MainActivity.this, SettingsActivity.class));
     }
 
-    public void onClickWind(View view) {
-        int windCounter = Integer.parseInt(windSpeedInfoView.getText().toString());
-        String newWindSpeed = String.valueOf(++windCounter);
-        windSpeedInfoView.setText(newWindSpeed);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == choseCityActivityRequestCode && resultCode == RESULT_OK) {
+            chosenCityView.setText(data.getStringExtra(ChoseCityActivity.cityTextKey));
+            dayDegreesView.setText(data.getStringExtra(ChoseCityActivity.dayTempTextKey));
+            nightDegreesView.setText(data.getStringExtra(ChoseCityActivity.nightTempTextKey));
+            atmospherePressureInfoView.setText(data.getStringExtra(ChoseCityActivity.atmospherePressureTextKey));
+            windSpeedInfoView.setText(data.getStringExtra(ChoseCityActivity.windSpeedTextKey));
+            humidityInfoView.setText(data.getStringExtra(ChoseCityActivity.humidityTextKey));
+            weekWeatherUrlView.setText(data.getStringExtra(ChoseCityActivity.weekWeatherTextKey));
+        }
     }
 
-    public void onClickHumidity(View view) {
-        int humidityCounter = Integer.parseInt(humidityInfoView.getText().toString());
-        String newHumidity = String.valueOf(++humidityCounter);
-        humidityInfoView.setText(newHumidity);
-    }
-
-    public void onClickPressure(View view) {
-        int pressureCounter = Integer.parseInt(atmospherePressureInfoView.getText().toString());
-        String newPressure = String.valueOf(++pressureCounter);
-        atmospherePressureInfoView.setText(newPressure);
-    }
-
-    public void onClickNightTemp(View view) {
-        int nightDegrees = Integer.parseInt(nightDegreesView.getText().toString());
-        String newNightTemp = String.valueOf(++nightDegrees);
-        nightDegreesView.setText(newNightTemp);
-    }
-
-    public void onClickDayTemp(View view) {
-        int dayDegrees = Integer.parseInt(dayDegreesView.getText().toString());
-        String newDayTemp = String.valueOf(++dayDegrees);
-        dayDegreesView.setText(newDayTemp);
+    public void onClickWeekWeather(View view) {
+        Uri uri = Uri.parse(weekWeatherUrlView.getText().toString());
+        Intent weekWeatherIntent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(weekWeatherIntent);
     }
 }
